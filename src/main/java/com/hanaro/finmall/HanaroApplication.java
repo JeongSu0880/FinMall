4c377af5e45c738d55e7e51b8b1146d5c1e4bcb3
Jeongsu <latsyrc900@gmail.com>
2026-03-17 11:02:43 +0900

    Chore: security.yml 추가, build.gradle에 로거 추가

--- src/main/java/com/hanaro/finmall/HanaroApplication.java
+++ src/main/java/com/hanaro/finmall/HanaroApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HanaroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HanaroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HanaroApplication.class, args);
+    }
 
 }
